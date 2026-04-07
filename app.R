2374424ad8f8ccb751237f52384ea428c3c7b278
Rafael Cardoso <67767381+rafael747cardoso@users.noreply.github.com>
2026-04-07 03:19:14 -0300

    all dropdowns

--- app.R
+++ app.R
@@ -1,4 +1,8 @@
 
+# Load functions ----
+lapply(list.files("funcs", pattern = "\\.R$", full.names = TRUE), source)
+
+
 # Frontend ----
 ui <- shiny::fluidPage(
   
@@ -33,12 +37,14 @@ ui <- shiny::fluidPage(
   shiny::uiOutput(outputId = "establishment_type_ui"),
 
   ### Establishment Name ----
+  shiny::uiOutput(outputId = "establishment_name_ui"),
   
   ### Purchase Type ----
+  shiny::uiOutput(outputId = "purchase_type_ui"),
   
   ### Purchase Content ----
-  
-  
+  shiny::uiOutput(outputId = "purchase_content_ui"),
+
   ## Button to send email ----
   shinyWidgets::actionBttn(
     inputId = "btn_send_email",
@@ -47,7 +53,7 @@ ui <- shiny::fluidPage(
   ),
   
   ## Display photo ----
-  shiny::imageOutput(outputId = "uploaded_photo"),
+  shiny::imageOutput(outputId = "uploaded_photo")
   
 )
 
@@ -58,35 +64,47 @@ server <- function(input, output, session) {
   ## Read categories from repo data ----
   r <- shiny::reactiveValues()
   r$opts_establishment_type <- data.table::fread("data/establishment_type.csv")$category
+  r$opts_establishment_name <- data.table::fread("data/establishment_name.csv")$category
+  r$opts_purchase_type      <- data.table::fread("data/purchase_type.csv")$category
+  r$opts_purchase_content   <- data.table::fread("data/purchase_content.csv")$category
   
   ## Build dropdowns ----
-  
-  ### Establishment Type ----
   output$establishment_type_ui <- shiny::renderUI({
-    shiny::selectizeInput(
-      inputId = "establishment_type",
-      label = "Establishment Type",
-      choices = r$opts_establishment_type,
-      options = list(
-        placeholder = "Search or create",
-        create = TRUE
-      )
-    )
+    fct_custom_select("Establishment Type", r)
+  })
+  output$establishment_name_ui <- shiny::renderUI({
+    fct_custom_select("Establishment Name", r)
+  })
+  output$purchase_type_ui <- shiny::renderUI({
+    fct_custom_select("Purchase Type", r)
+  })
+  output$purchase_content_ui <- shiny::renderUI({
+    fct_custom_select("Purchase Content", r)
   })
 
   ## Update categories ----
   shiny::observeEvent(input$establishment_type, {
     fct_update_cats(input_cat = input$establishment_type, 
                     var_name = "establishment_type",
-                    r = r,
-                    session = session)
+                    r = r, session = session)
+  })
+  shiny::observeEvent(input$establishment_name, {
+    fct_update_cats(input_cat = input$establishment_name, 
+                    var_name = "establishment_name",
+                    r = r, session = session)
+  })
+  shiny::observeEvent(input$purchase_type, {
+    browser()
+    fct_update_cats(input_cat = input$purchase_type, 
+                    var_name = "purchase_type",
+                    r = r, session = session)
+  })
+  shiny::observeEvent(input$purchase_content, {
+    fct_update_cats(input_cat = input$purchase_content, 
+                    var_name = "purchase_content",
+                    r = r, session = session)
   })
 
-  
-  
-  
-    
-  
   ## Display the photo for checking ----
   output$uploaded_photo <- shiny::renderImage({
     
@@ -106,7 +124,9 @@ server <- function(input, output, session) {
     
     req(
       input$receipt_photo,
-      input$establishment_type
+      input$establishment_type,
+      input$establishment_name,
+      input$purchase_type
     )
 
     ### Build file name ----
@@ -122,12 +142,20 @@ server <- function(input, output, session) {
     photo_name <- paste0(photo_name, "; ", input$establishment_type)
     
     #### Where: Establishment Name ----
+    photo_name <- paste0(photo_name, "; ", input$establishment_name)
     
     #### What: Purchase Type ----
+    photo_name <- paste0(photo_name, "; ", input$purchase_type)
     
     #### What: Purchase Content ----
-    # to use when the receipt has no info on the content purchased
-    
+    # to use when the receipt has no info on the content of the purchase
+    if (!is.null(input$purchase_content)) {
+      purchase_content <- trimws(input$purchase_content)
+      if (purchase_content != "") {
+        photo_name <- paste0(photo_name, "; ", purchase_content)
+      }
+    }
+
     ### TEST: save to PC ----
     file.copy(
       from = input$receipt_photo$datapath,
@@ -135,7 +163,7 @@ server <- function(input, output, session) {
     )
     
     ### Send email via Resend API ----
-  
+    
     
     
   })
---
+++ funcs/fct_custom_select.R
@@ -0,0 +1,16 @@
+
+fct_custom_select <- function(var_name, r){
+  
+  clean_var_name <- gsub(x = tolower(var_name), pattern = " ", replacement = "_")
+  
+  shiny::selectizeInput(
+    inputId = clean_var_name  ,
+    label = var_name,
+    choices = r[[paste0("opts_", clean_var_name)]],
+    options = list(
+      placeholder = "Search or create",
+      create = TRUE
+    )
+  )
+  
+}
